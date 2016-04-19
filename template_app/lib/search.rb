eb26e1c2aaa7d9f96b6dab857763ef918925e839
Sarah McAlear <smcalear@pivotal.io>
2016-04-19 11:31:45 -0700

    reference correct path for files
    Signed-off-by: Gregg Van Hove <gvanhove@pivotal.io>

--- template_app/lib/search.rb
+++ template_app/lib/search.rb
@@ -7,11 +7,11 @@ module Bookbinder
     def self.call(env)
       client = Elasticsearch::Client.new log: true, url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
       query = env['QUERY_STRING'].split('&').detect { |s| s =~ /\Aq=/ }.split('=').last
-      query_options = YAML.load_file(File.expand_path('../search.yml', __FILE__))
+      query_options = YAML.load_file(File.expand_path('../../search.yml', __FILE__))
       query_options['query_string']['query'] = query
       results = client.search index: 'searching', body: query_options
 
-      erb = ERB.new(File.read(File.expand_path('../search-results.html.erb', __FILE__)))
+      erb = ERB.new(File.read(File.expand_path('../../search-results.html.erb', __FILE__)))
 
       search_data = {
         query: query,
